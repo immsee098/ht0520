c1a45c2dea6682d3735bedce698e1cbea3e698d3
immsee098 <immsee098@gmail.com>
2019-05-23 10:06:12 +0900

    1

--- act.js
+++ act.js
@@ -1,13 +1,30 @@
+// function allowDrop(ev) {
+//     ev.preventDefault();
+//   }
+  
+//   function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+//   }
+  
+//   function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+//   }
 function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("src", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var src = document.getElementById(ev.dataTransfer.getData("src"));
+  var srcParent = src.parentNode;
+  var tgt = ev.currentTarget.firstElementChild;
+
+  ev.currentTarget.replaceChild(src, tgt);
+  srcParent.appendChild(tgt);
+}
