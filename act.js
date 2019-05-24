8cfbcb1c580a335cc62689acded5d4fc72e6c3e6
immsee098 <immsee098@gmail.com>
2019-05-24 13:21:04 +0900

    .....

--- act.js
+++ act.js
@@ -1,11 +1,17 @@
-import html2canvas from 'html2canvas';
-
-$add.onclick
-
-html2canvas(document.querySelector("#capture")).then(canvas => {
-  document.body.appendChild(canvas)
-});
-
+function sample() {
+  var background = document.getElementById('example').style.background;
+      
+  html2canvas(document.getElementById('example'), {
+      useCORS: true, // 다른사이트의리소스가있을때활성화(그러나...Access-Control-Allow-Origin 필요)
+      onrendered: function(canvas) {
+          canvas.toBlob(function(blob) {
+              saveAs(blob, 'download.png');
+          });
+          
+          // $("#test").html('<img src=' + canvas.toDataURL("image/png") + '>');
+      }
+  });
+}
 function allowDrop(ev) {
   ev.preventDefault();
 }
@@ -22,4 +28,33 @@ function drop(ev) {
 
   ev.currentTarget.replaceChild(src, tgt);
   srcParent.appendChild(tgt);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
 }
